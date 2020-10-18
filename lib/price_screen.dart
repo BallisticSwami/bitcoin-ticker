dd660941198dc1a15bf75758603c64b15b38dfb9
BallisticSwami <sbahadkar@rocketmail.com>
2020-10-18 19:34:13 +0530

    Added code till vid7

---
+++ lib/price_screen.dart
@@ -0,0 +1,204 @@
+import 'package:bitcointicker/coin_data.dart';
+import 'package:bitcointicker/utilities/networking.dart';
+import 'package:bitcointicker/utilities/size_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String currentCurrency = 'USD';
+  List<double> conversionFactor = [1, 1, 1, 1, 1];
+  bool listDragged = false;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  String coinURL = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,litecoin,dogecoin,tether&vs_currencies=';
+
+  void _getExchangeRates() async {
+    var url = '$coinURL$currentCurrency';
+    NetworkHelper netHelper = NetworkHelper(url);
+    var data = await netHelper.getData();
+    setState(() {
+      conversionFactor[0] = data["bitcoin"]["${currentCurrency.toLowerCase()}"].toDouble();
+      conversionFactor[1] = data["ethereum"]["${currentCurrency.toLowerCase()}"].toDouble();
+      conversionFactor[2] = data["litecoin"]["${currentCurrency.toLowerCase()}"].toDouble();
+      conversionFactor[3] = data["tether"]["${currentCurrency.toLowerCase()}"].toDouble();
+      conversionFactor[4] = data["dogecoin"]["${currentCurrency.toLowerCase()}"].toDouble();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getExchangeRates();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+        key: _scaffoldKey,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.grey[900],
+          tooltip: 'Select Currency',
+          elevation: 1,
+          onPressed: () {
+            if (listDragged == true) {
+              Navigator.pop(context);
+              setState(() {
+                listDragged = false;
+              });
+            } else {
+              setState(() {
+                listDragged = true;
+              });
+              _scaffoldKey.currentState
+                  .showBottomSheet<Null>((BuildContext context) {
+                return Container(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: ListView.builder(
+                    itemCount: currenciesList.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(currenciesList[index]),
+                        onTap: () {
+                          setState(() {
+                            currentCurrency = currenciesList[index];
+                            _getExchangeRates();
+                            listDragged  = false;
+                            Navigator.pop(context);
+                          });
+                        },
+                      );
+                    },
+                  ),
+                );
+              });
+            }
+          },
+          child: Icon(Icons.attach_money),
+        ),
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.dark.copyWith(
+            statusBarColor: Colors.transparent,
+              systemNavigationBarColor: Colors.white,
+              systemNavigationBarIconBrightness: Brightness.dark),
+          child: Stack(
+            children: [
+              SafeArea(
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: SizedBox(
+                          child: Container(
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.only(
+                            left: SizeConfig.safeBlockHorizontal * 5, top: SizeConfig.safeBlockVertical*1.7),
+                        child: Text(
+                          'Coin\nTicker',
+                          style: TextStyle(
+                              fontSize: SizeConfig.safeBlockHorizontal * 6.5,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      )),
+                    ),
+                    SizedBox(
+                      height: SizeConfig.safeBlockVertical * 2.5,
+                    ),
+                    Expanded(
+                      flex: 8,
+                      child: SizedBox(
+                        child: Container(
+                          child: ListView.builder(
+                            itemCount: imgList.length,
+                            itemBuilder: (context, index) {
+                              return CoinCard(
+                                  imageName: imgList[index],
+                                  heading: imgList[index],
+                                  subtitle:
+                                      '1 ${cryptoList[index]} = ${conversionFactor[index]} $currentCurrency');
+                            },
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeIn,
+                color: Colors.black.withOpacity(listDragged ? 0.3 : 0),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class CoinCard extends StatefulWidget {
+  final String imageName;
+  final String heading;
+  final String subtitle;
+
+  CoinCard({@required this.imageName, @required this.heading, this.subtitle});
+
+  @override
+  _CoinCardState createState() => _CoinCardState();
+}
+
+class _CoinCardState extends State<CoinCard> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Container(
+      height: SizeConfig.safeBlockHorizontal * 30,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(
+            SizeConfig.safeBlockHorizontal * 5,
+            0,
+            SizeConfig.safeBlockHorizontal * 5,
+            SizeConfig.safeBlockVertical * 2),
+        child: Card(
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            elevation: 0,
+            child: Row(children: [
+              Image.asset(
+                'assets/${widget.imageName.toLowerCase()}.png',
+                fit: BoxFit.fill,
+              ),
+              SizedBox(
+                width: SizeConfig.safeBlockHorizontal * 5,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: SizeConfig.safeBlockVertical * 1),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.heading,
+                      style: TextStyle(
+                          fontSize: SizeConfig.safeBlockHorizontal * 5.2,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      height: SizeConfig.safeBlockVertical * 0.8,
+                    ),
+                    Text(widget.subtitle,
+                        style: TextStyle(
+                            fontSize: SizeConfig.safeBlockHorizontal * 3.8,
+                            fontWeight: FontWeight.w400)),
+                  ],
+                ),
+              )
+            ])),
+      ),
+    );
+  }
+}
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color kprimaryColor = Colors.deepPurpleAccent[400];
+
+Color kprimaryDarkColor = Color(0xff4F13DC);
+
+Color kprimaryLightColor = Color(0xffa255ff);
+
+class MyTheme {
+  static ThemeData kDropDownMenuTheme = ThemeData.dark().copyWith(
+    hintColor: Colors.white ,
+  );
+}
